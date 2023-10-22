adb0a8177f884ae44aa6a55d25470b08f029eda7
kikok <s1090374@gm.pu.edu.tw>
2023-10-22 15:39:00 +0800

    加入專案檔案。

---
+++ class03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace class03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan timePerNews = new TimeSpan(0, 15, 35);
+
+            
+            int numberOfNews = int.Parse(Console.ReadLine());
+
+            
+            TimeSpan totalTime = timePerNews * numberOfNews;
+
+           
+            int hours = totalTime.Hours;
+            int minutes = totalTime.Minutes;
+            int seconds = totalTime.Seconds;
+
+            Console.WriteLine($"寫{numberOfNews}篇新聞總共需要{hours}小時{minutes}分{seconds}秒");
+        }
+    }
+}
